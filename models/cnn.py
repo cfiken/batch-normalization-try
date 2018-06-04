cf6c751fa4150e089534126ffdb0da30355d655d
Kentaro Nakanishi <cfiken@gmail.com>
2018-06-05 01:25:44 +0900

    Add: dropout config

--- models/cnn.py
+++ models/cnn.py
@@ -33,9 +33,10 @@ class CNN:
         l_4 = tf.layers.max_pooling2d(inputs=l_3, pool_size=[2, 2], strides=2)
         l_4_flat = tf.reshape(l_4, [-1, 4096])
         l_5 = tf.layers.dense(inputs=l_4_flat, units=1024, activation=tf.nn.relu)
-        l_5 = tf.layers.dropout(inputs=l_5,
-                                     rate=config['dropout_rate'],  # todo set config
-                                     training=self.is_training)
+        if config['is_use_drop_out']:
+            l_5 = tf.layers.dropout(inputs=l_5,
+                                    rate=config['dropout_rate'],
+                                    training=self.is_training)
         l_6 = tf.layers.dense(inputs=l_5, units=10)
         self.logits = l_6
 
